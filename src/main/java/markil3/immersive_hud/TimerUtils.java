5e553f2e72682f9da3022c1077c2bbfcc23dcc95
Markil3 <75867393+Markil3@users.noreply.github.com>
2021-03-25 17:14:47 -0600

    Fixes a ClassCastException (#1)

--- src/main/java/markil3/immersive_hud/TimerUtils.java
+++ src/main/java/markil3/immersive_hud/TimerUtils.java
@@ -930,7 +930,7 @@ public class TimerUtils
         Entity tmp = mc.player.getRidingEntity();
         boolean changed = false;
 
-        if (tmp == null)
+        if (tmp == null || !(tmp instanceof LivingEntity))
         {
             mountHealth = -1;
             mountMaxHealth = -1;
