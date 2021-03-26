93548ae7a7c98b6a4a209cbb9808410c4c059173
Markil3 <75867393+Markil3@users.noreply.github.com>
2021-03-26 11:21:22 -0600

    Adds the ability to always show hands and crosshairs.
    Apparently, @rflambert doesn't want the hands to fade. It seemed like a good idea, so I added a configurable option to the main repo.

--- src/main/java/markil3/immersive_hud/ModMenu.java
+++ src/main/java/markil3/immersive_hud/ModMenu.java
@@ -130,12 +130,28 @@ public class ModMenu implements ModMenuApi
                     .setSaveConsumer(val -> instance.setCrosshairTime((int) (val * TICKS_PER_SECOND)))
                     .build());
 
+            general.addEntry(entryBuilder.startBooleanToggle(
+                            "option.immersive_hud.hideCrosshair",
+                    instance.hideCrosshair())
+                    .setDefaultValue(true)
+                    .setTooltip("tooltip.immersive_hud.hideCrosshair")
+                    .setSaveConsumer(val -> instance.hideCrosshair(val))
+                    .build());
+
             general.addEntry(entryBuilder.startDoubleField(
                     "option.immersive_hud.handTime",
                     (float) instance.getHandTime() / TICKS_PER_SECOND)
                     .setDefaultValue(30)
                     .setTooltip("tooltip.immersive_hud.handTime")
                     .setSaveConsumer(val -> instance.setHandTime((int) (val * TICKS_PER_SECOND)))
+                    .build());
+
+            general.addEntry(entryBuilder.startBooleanToggle(
+                    "option.immersive_hud.hideHands",
+                    instance.hideHands())
+                    .setDefaultValue(true)
+                    .setTooltip("tooltip.immersive_hud.hideHands")
+                    .setSaveConsumer(val -> instance.hideHands(val))
                     .build());
 
             general.addEntry(entryBuilder.startDoubleField(
